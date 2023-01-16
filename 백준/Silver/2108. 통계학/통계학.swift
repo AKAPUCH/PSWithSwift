cbb97db48b2d9ff654dfea9f06abb1e5a05a5c04
AKAPUCH <116094622+AKAPUCH@users.noreply.github.com>
2023-01-17 01:38:31 +0900

    [Silver III] Title: 통계학, Time: 132 ms, Memory: 94388 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.swift
@@ -0,0 +1,78 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let file = FileIO()
+
+var arr = [Int]()
+var counting = Array(repeating: 0, count: 8001)
+var diction = [Int:Int]()
+
+for _ in 0..<file.readInt() {
+    let x = file.readInt()
+    arr.append(x)
+    diction[x, default: 0]+=1
+}
+var avg = String(format: "%.f", Double(arr.reduce(0){$0+$1}) / Double(arr.count))
+print(avg == "-0" ? "0" : avg)
+print(arr.sorted()[arr.count/2])
+let max = diction.max(by: {$0.value<$1.value})!.value
+var ans = diction.filter{$0.value==max}.keys.sorted()
+print(ans.count>1 ? ans[1] : ans[0])
+print(arr.max()!-arr.min()!)
