be53dbca0fbcf327b06264ea0b0fb24a028f52c8
AKAPUCH <116094622+AKAPUCH@users.noreply.github.com>
2023-02-24 21:46:46 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.58 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.swift
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.swift
@@ -1,25 +1,23 @@
-
 func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    var newBoard = board
+    let newMoves = moves.map{$0 - 1}
     var box : [Int] = []
     var cnt = 0
-    var newBoard = board
-    var newMoves = moves.map{$0 - 1}
     let n = newBoard.count
+    
     for column in newMoves{
-    for row in 0..<n {
-        if newBoard[row][column] == 0 {continue}
-        let lastone = box.last
-        box.append(newBoard[row][column])
-        newBoard[row][column] = 0
-        if lastone == box.last {
-            cnt += 2
-            box.popLast()
-            box.popLast()
+        for row in 0..<n {
+            if newBoard[row][column] == 0 {continue}
+            let lastone = box.last
+            box.append(newBoard[row][column])
+            newBoard[row][column] = 0
+            if lastone == box.last {
+                cnt += 2
+                box.removeLast(2)
+            }
+            break
         }
-        break
     }
-        }
     
     return cnt
 }
-
