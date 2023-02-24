4119fa166373d30360e9e47515ba19cb71e3283e
AKAPUCH <116094622+AKAPUCH@users.noreply.github.com>
2023-02-24 21:00:12 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.93 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.swift
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.swift
@@ -1,39 +1,25 @@
-func swapArray(_ arr :  [[Int]], _ count : Int) -> [[Int]] {
-      var temp = arr
-      for i in 0..<count {
-      for j in i+1..<count {
-          (temp[i][j], temp[j][i]) = (temp[j][i], temp[i][j])
-      }
-    }
-    return temp
-}
-func pick(_ index : Int, _ arr : inout [[Int]])-> Int {
-    var ans = 0
-    for i in 0..<arr[index-1].count { 
-      if arr[index-1][i] == 0 {continue}
-      ans = arr[index-1][i]
-      
-      arr[index-1][i] = 0
-      break
-    }
-    return ans
-}
 
 func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
-    var stack : [Int] = []
-    var swapArr = swapArray(board, board.count)
+    var box : [Int] = []
     var cnt = 0
-    for i in moves {
-      let choice = pick(i, &swapArr)
-      if choice == 0 {continue}
-      if !stack.isEmpty{        
-        if stack.last! == choice {
-          stack.popLast()
-          cnt+=2
-          continue 
-          }               
+    var newBoard = board
+    var newMoves = moves.map{$0 - 1}
+    let n = newBoard.count
+    for column in newMoves{
+    for row in 0..<n {
+        if newBoard[row][column] == 0 {continue}
+        let lastone = box.last
+        box.append(newBoard[row][column])
+        newBoard[row][column] = 0
+        if lastone == box.last {
+            cnt += 2
+            box.popLast()
+            box.popLast()
+        }
+        break
     }
-    stack.append(choice)   
-    }
+        }
+    
     return cnt
 }
+
