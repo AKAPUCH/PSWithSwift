ec243105245e1d3fb9d62ba965a9a118d54db342
AKAPUCH <116094622+AKAPUCH@users.noreply.github.com>
2023-02-25 14:29:31 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.10 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.swift
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.swift
@@ -1,10 +1,10 @@
 var num : [Int] = []
 
-func multiply(_ c : String, _ seq : Int) {
+func multiply(_ c : String, _ seq : Int) { // 점수처리 함수
     switch c {
     case "#" : num[seq] *= -1
     case "*" :
-                if seq > 0 {num[seq-1] *= 2}
+                if seq > 0 {num[seq-1] *= 2} //첫 수일시 뒤에도 곱하기 2
                 num[seq] *= 2
     case "D" : num[seq] *= num[seq]
     case "T" : num[seq] *= (num[seq] * num[seq])
@@ -13,23 +13,23 @@ func multiply(_ c : String, _ seq : Int) {
 }
 
 func solution(_ dartResult:String) -> Int {
-    var idiot = dartResult.map{String($0)}
+    var idiot = dartResult.map{String($0)} //인덱스 편의를 위해 변환
     for k in 0..<idiot.count {
-        if k > 0 && idiot[k] == "0" && idiot[k-1] == "1" {
+        if k > 0 && idiot[k] == "0" && idiot[k-1] == "1" { //10이 나왔을 경우
             idiot[k-1] = "10"
-            idiot[k] = "F"
+            idiot[k] = "erase"
         }
     }
-    idiot = idiot.filter{$0 != "F"}
-    num = idiot.compactMap{Int($0)}
-    var cnt = -1
-
+    idiot = idiot.filter{$0 != "erase"} // 남아있는 0 제거
+    num = idiot.compactMap{Int($0)} // 숫자값만 뽑아내기
+    
+    var cnt = -1 // num배열의 인덱스
         for i in idiot {
-            if Int(i) != nil {
+            if Int(i) != nil { // 숫자라면
                 cnt += 1
                 continue
             }
             multiply(i, cnt)
     }
-    return num.reduce(0, +)
+    return num.reduce(0, +) //합 반환
 }
