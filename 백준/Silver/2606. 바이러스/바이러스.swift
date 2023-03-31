d69498094a2fe4f273201f6e492f0103074a5fdf
AKAPUCH <116094622+AKAPUCH@users.noreply.github.com>
2023-03-31 14:45:30 +0900

    [Silver III] Title: 바이러스, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.swift
+++ 백준/Silver/2606. 바이러스/바이러스.swift
@@ -1,26 +1,23 @@
+//DFS
 let v = Int(readLine()!)!
 let e = Int(readLine()!)!
+var nodeArr : [[Int]] = Array(repeating: [], count: v+1)
+var visitedVertex = Array(repeating: false, count: v+1)
 var cnt = 0
-var vertexArr : [[Int]] = Array(repeating : [], count : v+1)
 for _ in 0..<e {
-  let edge = readLine()!.split(separator: " ").map{Int(String($0))!}
-  vertexArr[edge[0]].append(edge[1]) // 간선을 저장
-  vertexArr[edge[1]].append(edge[0]) // 무방향이므로 반대방향도 넣기
+    let node = readLine()!.split(separator: " ").map{Int(String($0))!}
+    nodeArr[node[0]].append(node[1])
+    nodeArr[node[1]].append(node[0])
 }
-var visitCheckArr : [Bool] = Array(repeating: false, count: v+1)
-var queue : [Int] = []
-queue.append(1)
-visitCheckArr[1] = true
-while !queue.isEmpty {
-  
-  
-  let visitVertex = queue.removeFirst()
-  for neighborVertex in vertexArr[visitVertex] {
-    if visitCheckArr[neighborVertex] {continue}
-    visitCheckArr[neighborVertex] = true
-    cnt += 1
-    queue.append(neighborVertex)
-  }
-}
+dfs(1)
+print(cnt-1)
 
-print(cnt)
+func dfs(_ vertex : Int) {
+    cnt += 1
+    visitedVertex[vertex] = true
+    for neighborVertex in nodeArr[vertex] {
+        if visitedVertex[neighborVertex] {continue}
+        dfs(neighborVertex)
+    }
+    return
+}
